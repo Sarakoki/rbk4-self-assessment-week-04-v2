ea69d305550a6f7b19b5f541e7a5eec7d8a90923
sarakoki <sarakoki94@gmail.com>
2018-04-07 09:30:00 +0300

    solve

--- node-req-count/server.js
+++ node-req-count/server.js
@@ -3,15 +3,27 @@ var http = require('http');
 var path = require('path');
 
 var globalCounter = {};
-
-var server = http.createServer(function(request, response) {
+var server = http.createServer(function(request, response,callback) {
   var endpoint = url.parse(request.url, true).pathname;
   var property = endpoint.replace(/^\//, '');
-
+var counter = 0;
   if (request.method === 'POST') {
-    // YOUR CODE HERE
+    // YOUR CODE HERE 
+    if (endpoint === '/') {
+      return null;
+    } else
+    request.on('endpoint', function(data) {
+      endpoint += data;
+      response.on('end', function() {
+        callback(globalCounter[endpoint] = ++counter)
+      })
+    });
+
   } else if (request.method === 'GET') {
     // YOUR CODE HERE
+    response.writeHead(statusCode, globalCounter);
+    response.append(globalCounter[endpoint]);
+     response.end(globalCounter.endpoint)
   } else {
     response.statusCode = 404;
     response.end();
